cb2790c2828a2e5d947d0eacd390754c12b36cd5
AnyelySC <surcogreissy@gmail.com>
2026-07-09 08:40:25 -0500

    Categoria A.

--- src/Libreria/Categoria.java
+++ src/Libreria/Categoria.java
@@ -4,10 +4,37 @@
  */
 package Libreria;
 
-/**
- *
- * @author romin
- */
 public class Categoria {
+
+    private int codigo;
+    private String nombre;
+
+    public Categoria() {
+    }
+
+    public Categoria(int codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
     
+    @Override
+    public String toString() {
+        return "Categoría: " + nombre + " (Código: " + codigo + ")";
+    }
 }
