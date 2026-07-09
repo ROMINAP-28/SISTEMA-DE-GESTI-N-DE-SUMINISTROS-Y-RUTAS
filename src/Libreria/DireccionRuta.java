3b345e5e63c625f93227f5f50bb1b896ce63d90f
AnyelySC <surcogreissy@gmail.com>
2026-07-09 08:43:59 -0500

    DireccionRuta A.
    Added constructor, getters, setters, and toString method.

--- src/Libreria/DireccionRuta.java
+++ src/Libreria/DireccionRuta.java
@@ -4,10 +4,37 @@
  */
 package Libreria;
 
-/**
- *
- * @author romin
- */
 public class DireccionRuta {
+
+    private int codigo;
+    private String nombre;
+
+    public DireccionRuta() {
+    }
+
+    public DireccionRuta(int codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
     
+    @Override
+    public String toString() {
+        return nombre;
+    }
 }
