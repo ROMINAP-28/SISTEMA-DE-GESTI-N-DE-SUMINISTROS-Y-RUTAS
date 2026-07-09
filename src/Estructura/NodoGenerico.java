6f995e619afdf67a5bce22fa1f397ef8113a48ef
AnyelySC <surcogreissy@gmail.com>
2026-07-09 09:33:40 -0500

    NodoGenerico A.

--- src/Estructura/NodoGenerico.java
+++ src/Estructura/NodoGenerico.java
@@ -2,12 +2,67 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package Estructura;
 
-/**
- *
- * @author romin
- */
-public class NodoGenerico {
-    
+public class NodoGenerico<T> {
+    private T dato;
+    private NodoGenerico<T> siguiente;
+    private NodoGenerico<T> anterior;
+    private NodoGenerico<T> izquierdo;
+    private NodoGenerico<T> derecho;
+    private int altura;
+
+    public NodoGenerico() {
+    }
+
+    public NodoGenerico(T dato) {
+        this.dato = dato;
+        this.altura = 1; 
+    }
+
+    public T getDato() { 
+        return dato; 
+    }
+    public void setDato(T dato) { 
+        this.dato = dato; 
+    }
+
+    public NodoGenerico<T> getSiguiente() { 
+        return siguiente; 
+    }
+    public void setSiguiente(NodoGenerico<T> siguiente) { 
+        this.siguiente = siguiente; 
+    }
+
+    public NodoGenerico<T> getAnterior() { 
+        return anterior; 
+    }
+    public void setAnterior(NodoGenerico<T> anterior) { 
+        this.anterior = anterior; 
+    }
+
+    public NodoGenerico<T> getIzquierdo() { 
+        return izquierdo; 
+    }
+    public void setIzquierdo(NodoGenerico<T> izquierdo) { 
+        this.izquierdo = izquierdo; 
+    }
+
+    public NodoGenerico<T> getDerecho() { 
+        return derecho; 
+    }
+    public void setDerecho(NodoGenerico<T> derecho) { 
+        this.derecho = derecho; 
+    }
+
+    public int getAltura() { 
+        return altura; 
+    }
+    public void setAltura(int altura) { 
+        this.altura = altura; 
+    }
 }
