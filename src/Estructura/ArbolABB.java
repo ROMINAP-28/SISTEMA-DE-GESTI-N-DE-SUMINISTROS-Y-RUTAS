7730893e3de74062145b4f95661114d54c551657
AnyelySC <surcogreissy@gmail.com>
2026-07-10 13:20:03 -0500

    Update ArbolABB.java

--- src/Estructura/ArbolABB.java
+++ src/Estructura/ArbolABB.java
@@ -1,13 +1,114 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+//Anyely
 package Estructura;
 
-/**
- *
- * @author romin
- */
-public class ArbolABB {
-    
+public class ArbolABB<T extends Comparable<T>> {
+    private NodoGenerico<T> raiz;
+
+    public ArbolABB() {
+        raiz = null;
+    }
+
+    public boolean estaVacio() {
+        return raiz == null;
+    }
+
+    public void insertar(T dato) {
+        raiz = insertarRecursivo(raiz, dato);
+    }
+
+    private NodoGenerico<T> insertarRecursivo(NodoGenerico<T> nodo, T dato) {
+        if (nodo == null) {
+            return new NodoGenerico<>(dato);
+        }
+
+        if (dato.compareTo(nodo.getDato()) < 0) {
+            nodo.setIzquierdo(
+                    insertarRecursivo(nodo.getIzquierdo(), dato)
+            );
+
+        } else if (dato.compareTo(nodo.getDato()) > 0) {
+            nodo.setDerecho(
+                    insertarRecursivo(nodo.getDerecho(), dato)
+            );
+        }
+
+        return nodo;
+    }
+
+    public T buscar(T dato) 
+        NodoGenerico<T> encontrado = buscarRecursivo(raiz, dato);
+
+        if (encontrado != null) {
+            return encontrado.getDato();
+        }
+
+        return null;
+    }
+
+    private NodoGenerico<T> buscarRecursivo(NodoGenerico<T> nodo, T dato) {
+        if (nodo == null) {
+            return null;
+        }
+
+        if (dato.equals(nodo.getDato())) {
+            return nodo;
+        }
+
+        if (dato.compareTo(nodo.getDato()) < 0) {
+            return buscarRecursivo(
+                    nodo.getIzquierdo(),
+                    dato
+            );
+
+        } else {
+            return buscarRecursivo(
+                    nodo.getDerecho(),
+                    dato
+            );
+        }
+    }
+
+    public void inOrden() {
+        inOrdenRecursivo(raiz);
+    }
+
+    private void inOrdenRecursivo(NodoGenerico<T> nodo) {
+        if (nodo != null) {
+            inOrdenRecursivo(nodo.getIzquierdo());
+            System.out.println(nodo.getDato());
+            inOrdenRecursivo(nodo.getDerecho());
+        }
+    }
+
+    public void preOrden() {
+        preOrdenRecursivo(raiz);
+    }
+
+    private void preOrdenRecursivo(NodoGenerico<T> nodo) {
+        if (nodo != null) {
+            System.out.println(nodo.getDato());
+            preOrdenRecursivo(nodo.getIzquierdo());
+            preOrdenRecursivo(nodo.getDerecho());
+        }
+    }
+
+    public void postOrden() {
+        postOrdenRecursivo(raiz);
+    }
+
+    private void postOrdenRecursivo(NodoGenerico<T> nodo) {
+        if (nodo != null) {
+            postOrdenRecursivo(nodo.getIzquierdo());
+            postOrdenRecursivo(nodo.getDerecho());
+            System.out.println(nodo.getDato());
+        }
+    }
+
+    public NodoGenerico<T> getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(NodoGenerico<T> raiz) {
+        this.raiz = raiz;
+    }
 }
