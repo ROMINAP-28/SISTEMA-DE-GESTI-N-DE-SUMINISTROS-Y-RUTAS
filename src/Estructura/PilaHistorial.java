ffb392c96819548372e5a2e67e53aca639914cfa
AnyelySC <surcogreissy@gmail.com>
2026-07-10 01:00:23 -0500

    Refactor PilaHistorial to support generics
    Refactor PilaHistorial class to use generics and add methods for stack operations.

--- src/Estructura/PilaHistorial.java
+++ src/Estructura/PilaHistorial.java
@@ -1,10 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+//Anyely
 package Estructura;
 
+public class PilaHistorial<T> {
+    private NodoGenerico<T> cima;
 
-public class PilaHistorial {
-    
+    public PilaHistorial() {
+        cima = null;
+    }
+
+    public boolean estaVacia() {
+        return cima == null;
+    }
+
+    public void push(T dato) {
+        NodoGenerico<T> nuevo = new NodoGenerico<>(dato);
+        nuevo.setSiguiente(cima);
+        cima = nuevo;
+    }
+
+    public T pop() {
+        if (estaVacia()) {
+            return null;
+        }
+
+        T dato = cima.getDato();
+        cima = cima.getSiguiente();
+        return dato;
+    }
+
+    public T peek() {
+        if (estaVacia()) {
+            return null;
+        }
+        return cima.getDato();
+    }
+
+    public void mostrar() {
+        NodoGenerico<T> aux = cima;
+        while(aux != null) {
+            System.out.println(aux.getDato());
+            aux = aux.getSiguiente();
+        }
+    }
+
+    public NodoGenerico<T> getCima() {
+        return cima;
+    }
+
+    public void setCima(NodoGenerico<T> cima) {
+        this.cima = cima;
+    }
 }
