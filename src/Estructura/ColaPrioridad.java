c13b04eca43df05f930b12625e5a15207f2a64da
AnyelySC <surcogreissy@gmail.com>
2026-07-10 00:57:06 -0500

    Update ColaPrioridad.java

--- src/Estructura/ColaPrioridad.java
+++ src/Estructura/ColaPrioridad.java
@@ -1,13 +1,71 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+//Anyely
 package Estructura;
 
-/**
- *
- * @author romin
- */
-public class ColaPrioridad {
+public class ColaPrioridad<T> {
+    private NodoGenerico<T> frente;
     
+    public ColaPrioridad() {
+        frente = null;
+    }
+
+    public boolean estaVacia() {
+        return frente == null;
+    }
+
+    public void insertar(T dato) {
+        NodoGenerico<T> nuevo = new NodoGenerico<>(dato);
+
+        if (estaVacia()) {
+            frente = nuevo;
+
+        } else {
+            NodoGenerico<T> aux = frente;
+
+            while (aux.getSiguiente() != null) {
+                aux = aux.getSiguiente();
+            }
+
+            aux.setSiguiente(nuevo);
+            nuevo.setAnterior(aux);
+        }
+    }
+
+    public T eliminar() {
+        if (estaVacia()) {
+            return null;
+        }
+
+        T dato = frente.getDato();
+        frente = frente.getSiguiente();
+
+        if (frente != null) {
+            frente.setAnterior(null);
+        }
+
+        return dato;
+    }
+
+    public T primero() {
+        if (estaVacia()) {
+            return null;
+        }
+        return frente.getDato();
+    }
+
+    public void mostrar() {
+        NodoGenerico<T> aux = frente;
+
+        while (aux != null) {
+            System.out.println(aux.getDato());
+            aux = aux.getSiguiente();
+        }
+    }
+
+    public NodoGenerico<T> getFrente() {
+        return frente;
+    }
+
+    public void setFrente(NodoGenerico<T> frente) {
+        this.frente = frente;
+    }
 }
